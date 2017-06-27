d5cd43ac53d1f89ddf65f7d9e4c673fcbe6dc10a
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-06-27 12:45:21 +0100

    updated version

--- till.js
+++ till.js
@@ -1,6 +1,18 @@
 var apples = { 
-    name:"apples",
+    name: "Apples",
     price:0.99
 }
 
-var products =[apples, oranges, babanas,mangoes,pears]
+var products =["apples", "oranges", "bananas","mangoes","pears"]
+
+for ( var productIndex=0; productIndex < products.length; productIndex ++) {
+
+    var buttonsContainer = document.getElementById('buttons')
+
+    var 
+
+    buttonsContainer.innerHTML += productIndex
+
+}
+
+
