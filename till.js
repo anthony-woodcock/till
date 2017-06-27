dd9880a30c6c10d1dfd892e05b48097cef65ccd6
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-06-27 15:20:46 +0100

    final till JS, with price numbers

--- till.js
+++ till.js
@@ -1,101 +1,95 @@
-var apples = { 
-    name: "Apples",
-    price:0.99
-}
-
-var oranges = {
-    name: "Oranges",
-    price:0.50
+var apples = {
+  name: 'Apples',
+  price: 0.50
 }
 
 var bananas = {
-    name: "Bananas",
-    price:0.40
+  name: 'Bananas',
+  price: 0.20
 }
 
-var mangoes = {
-    name: "Mangoes",
-    price:0.70
+var oranges = {
+  name: 'Oranges',
+  price: 0.30
 }
 
 var pears = {
-    name: "Pears",
-    price:0.30
+  name: 'Pears',
+  price: 0.40
 }
 
-var products =["apples", "oranges", "bananas","mangoes","pears"]
+var mangoes = {
+  name: 'Pears',
+  price: 0.60
+}
 
-for ( var productIndex = 0; productIndex < products.length; productIndex ++) {
+var products = [apples, bananas, oranges, pears, mangoes]
 
-    var buttonsContainer = document.getElementById('buttons')
+for (var productIndex = 0; productIndex < products.length; productIndex++) {
+  var buttonsContainer = document.getElementById('buttons')
+  var product = products[productIndex]
 
-    var product = products[productIndex]
+  var buttonsHTML = '<div class="col-md-2">'
+  buttonsHTML += '<div class="panel panel-default">'
+  buttonsHTML += '<div class="panel-heading">'
+  buttonsHTML += '<h3 class="panel-title">' + product.name + '</h3>'
+  buttonsHTML += '</div>'
+  buttonsHTML += '<div class="panel-body">'
+  buttonsHTML += '<p>'
+  buttonsHTML += '<h3 class="panel-title">' + product.price + '</h3>'
+  buttonsHTML += '</p>'
+  buttonsHTML += '<button type="button" class="btn btn-primary" onclick="transaction.add(\'' + product.name + '\')">Add</button>'
+  buttonsHTML += '</div>'
+  buttonsHTML += '</div>'
+  buttonsHTML += '</div>'
 
-      var buttonsHTML = '<div class="col-md-2">'
-        buttonsHTML +=      '<div class="panel panel-default">'
-        buttonsHTML +=        '<div class="panel-heading">'
-        buttonsHTML +=          '<h3 class="panel-title">' + product.name + '</h3>'
-        buttonsHTML +=        '</div>'
-        buttonsHTML +=        '<div class="panel-body">'
-        buttonsHTML +=          '<p>'
-        buttonsHTML +=            '<strong>Price: </strong> £0.99'
-        buttonsHTML +=          '</p>'
-        buttonsHTML +=          '<button type="button" class="btn btn-primary" onclick="till.add(\'' + product.name + '\')">Add</button>'
-        buttonsHTML +=        '</div>'
-        buttonsHTML +=      '</div>'
-        buttonsHTML +=    '</div>'
-
-    buttonsContainer.innerHTML += buttonsHTML
-
+  buttonsContainer.innerHTML += buttonsHTML
 }
 
 var transaction = {}
-    transaction.items = []
-    transaction.add = function (productName) {
-        for (productIndex = 0; productIndex < products.length; productIndex ++) {
-            var currentProduct = products[productIndex]
+transaction.items = []
+transaction.add = function (productName) {
+  for (productIndex = 0; productIndex < products.length; productIndex++) {
+    var currentProduct = products[productIndex]
 
-            if (currentProduct.name === productName) {
-                var selectedProduct = currentProduct
-                break
-            }
-        }
+    if (currentProduct.name === productName) {
+      var selectedProduct = currentProduct
+      break
+    }
+  }
 
-        if (selectedProduct) {
-            var newItem = {
-                id: (Math.random() * 100000),
-                name: selectedProduct.name,
-                price: selectedProduct.price
-            }
-
-            this.items.push(newItem)   
-
-            updateDOM()
-        }
-}
-
-function updateDom() {
-
-    var tillItems = document.getElementById('tillItems')
-    tillItems.innerHTML = ''
-
-    var total = 0.00
-
-    for (var itemIndex = 0; itemIndex < transaction.items.length; intemIndex ++) {
-
-        var currentItem = transaction.items[itemIndex]
-
-        var itemsHTML = '<li class="list-grou-item">'
-        itemsHTML += '<span class="badge">' + currentItem.price + '</span>'
-        itemsHTML += currentItem.name
-        itemsHTML += '</li>'
-
-        tillItems.innerHTML += itemsHTML
-        total += currentItem.price
-
+  if (selectedProduct) {
+    var newItem = {
+      id: Math.floor(Math.random() * 100000),
+      name: selectedProduct.name,
+      price: selectedProduct.price
     }
 
-    document.getElementById('total').innerHTML = total.toFixed(2)
+    this.items.push(newItem)
+    updateDOM()
+  }
+}
 
+
+function updateDOM () {
+  var tillItems = document.getElementById('tillItems')
+  tillItems.innerHTML = ''
+
+  var total = 0.00
+
+
+  for (var itemIndex = 0; itemIndex < transaction.items.length; itemIndex++) {
+    var currentItem = transaction.items[itemIndex]
+
+    var itemsHTML = '<li class="list-group-item">'
+    itemsHTML += '<span class="badge">' + currentItem.price + '</span>'
+    itemsHTML += currentItem.name
+    itemsHTML += '</li>'
+
+    tillItems.innerHTML += itemsHTML
+    total += currentItem.price
+  }
+
+  document.getElementById('total').innerHTML = total.toFixed(2)
 }
 
